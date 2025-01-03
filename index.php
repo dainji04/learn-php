6880881bd185f7580bb951d1d64da17be5c892b8
dainji04 <ndai31072004@gmail.com>
2025-01-03 20:37:31 +0700

    destruct

--- index.php
+++ index.php
@@ -11,13 +11,22 @@ class Car
         $this->engine = $engine;
         $this->weight = $weight;
     }
+    public function __destruct()
+    {
+        echo 'Car has been destroyed';
+    }
     public function __Echo()
     {
         echo $this->model . ':' . PHP_EOL;
         echo '  ' . $this->engine . PHP_EOL;
         echo '  Weight: ' . $this->weight . PHP_EOL;
     }
+    static function __Echo2()
+    {
+        echo 'Hello';
+    }
 }
 echo "<pre>";
 $lamborini = new Car('Lamborini', 'V12', '1500');
 $lamborini->__Echo();
+$lamborini::__Echo2();
